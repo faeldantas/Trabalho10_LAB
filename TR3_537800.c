7886541720f5fe0f97b6b539d2b4ec1ce61c8587
faeldantas <109675875+faeldantas@users.noreply.github.com>
2022-12-11 14:32:40 -0300

    Update TR3_537800.c

--- TR3_537800.c
+++ TR3_537800.c
@@ -19,22 +19,23 @@ int main(){
     for (y = 0; y <= 199; y++) {
         x[y] = 0;
     }    
-
+    // Pede que o usuário digite um número. Até que o número seja -1, todos os números serão armazenados em um vetor 
+    // de bit. Ao final, são exibidos na tela em ordem.
     while (1) {
         printf("Digite um número: ");
         scanf("%d", &z);
 
-        if (z == -1) break;
-        if (z < 0 || z > 5000) {
+        if (z == -1) break;	// verifica se o número recebido é válido
+        if (z < 0 || z > 5000) {// e se ele está no intervalo pré-estabelecido.
             printf("Número inválido!\n");
             continue;
         }
 
-        x[z / 32] |= (1 << (z % 32));
+        x[z / 32] |= (1 << (z % 32));// 
     }
 
     printf("Números digitados: ");
-
+    //O vetor é percorrido fazendo comparações bit a bit a fim de verificar quais foram digitados.
     for (k = 0; k <= 5000; k++) {
         if (x[k / 32] & (1 << (k % 32))) {
             printf("%d ", k);
